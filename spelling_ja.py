02285b0164ac7781b21e63f5f621989422d11b26
Alexei Sholik <alcosholik@gmail.com>
2011-12-04 18:26:04 +0200

    Basic implementation of the Japanese spelling

---
+++ spelling_ja.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Japanese rules and tables for the 'spell' module"""
+
+RULES = [
+    '1x = 十{x}',
+    'ab = {a}十{b}',
+    '1xx = 百{x}',
+    'axx = {a}百{x}',
+    'axxx = {a}千{x}',
+    '(a)xxxx = {a}{x}',
+]
+
+NUMBERS = {
+    0: '零',
+    1: '一',
+    2: '二',
+    3: '三',
+    4: '四',
+    5: '五',
+    6: '六',
+    7: '七',
+    8: '八',
+    9: '九',
+    10: '十',
+}
+
+ORDERS = [
+    '', '万', '億', 'billón', 'trillón', 'quadrillón',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion',
+    'decillion', 'undecillion', 'duodecillion', 'tredecillion',
+    'quattuordecillion', 'quindecillion'
+]
+
+ORDER_SEP = ''
