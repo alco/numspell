83df55986ce4b67b7a34c592fcdea12f759f0a95
Alexei Sholik <alcosholik@gmail.com>
2011-12-08 19:44:15 +0200

    Update the draft Russian rules

--- spelling_es.py
+++ spelling_es.py
@@ -105,6 +105,8 @@ def pl_2(order):
 
 RU_PASSES = """
 ^ 1 <order> = <order>
+1 <thousand> = одна тысяча
+2 <thousand> = две тысячи
 <2_to_4> <order> = <order, pl_1>
 <not_1> <order> = <order, pl_2>
 """
