9444870f5a8a47125254f60b6efb2fd7cd564468
Alexei Sholik <alcosholik@gmail.com>
2011-12-09 20:59:57 +0200

    Refactor principal classes of the lisp module; add docstrings

--- lisp.py
+++ lisp.py
@@ -1,78 +1,114 @@
-"""Regular expressions for lists"""
+"""List processing based on a small formatting language
 
-from functools import reduce
-from operator import and_
+Create an instance of LispObject and then use its `sub` method to replace a
+groups of elements of a list with one new element, based on the format string
+and the substitution dictionary passed to the instance's constructor.
+
+"""
+
 import re
 
 
 class MatchObject(object):
-    def __init__(self, start, end):
-        self._start = start
-        self._end = end
+    """This class encapsulates the range of matched elements"""
 
-    def start(self):
-        return self._start
+    def __init__(self, start, length):
+        """Simply store `start` and calculate `end` and calculate `span`
 
-    def end(self):
-        return self._end
+        Arguments:
+            start  -- index of the first elements in the matching sequence
+            length -- length of the sequence
 
-    def span(self):
-        return self._start, self._end
+        """
+        self.start = start
+        self.end = start + length - 1
+        self.span = (start, self.end)
 
 
-class ListreObject(object):
-    def __init__(self, rule, meta=None):
+class LispObject(object):
+    """This is a list processor used to perform element replacement
+
+    Initialize it with a format string and a `meta` dictionary which contains
+    functions for searching and replacing the tokens mentioned in the format
+    string.
+
+    The method for replacing elements in a list is `sub`.
+
+    The `search` method returns a MatchObject containing the range  the format string.
+
+    """
+
+    def __init__(self, format_str, meta=None):
+        """Initialize the instance with a format string and a `meta` dictionary
+
+        Arguments:
+            format_str -- format string written in the special syntax
+
+            meta       -- dictionary containing functions needed for searching,
+                          replacing and transforming list elements
+
+        """
         self.meta = meta or {}
-        self.pattern, self.body, self.mapping = _process_rule(rule, self.meta)
-
-    def match(self, list_):
-        cmp_fun = lambda f, x: f is None or f(x)
-
-        if self.pattern[0] == '^':
-            # Match only at the beginning of list_
-            blist = map(cmp_fun, self.pattern[1:], list_)
-            # Return a new match object if and only if all of the list elements
-            # are True
-            if reduce(and_, blist):
-                m = MatchObject(0, len(self.pattern) - 2)
-                return m
-            return None
-
-        list_len = len(list_)
-        pat_len = len(self.pattern)
-        if list_len < pat_len:
-            return None
-
-        for i in range(list_len - pat_len + 1):
-            blist = map(cmp_fun, self.pattern, list_[i:])
-            if reduce(and_, blist):
-                m = MatchObject(i, i + pat_len - 1)
-                return m
-
-        return None
+        pattern, body, mapping = _process_string(format_str, self.meta)
+        self.pattern, self.body, self.mapping = pattern, body, mapping
 
     def sub(self, list_):
+        """Perform substitution and return a new list along with matches
+
+        Each sequence of elements matching to the pattern defined in the format
+        string will be replaced by exactly one element the contents of which is
+        based on the format string and the matched elements.
+
+        Return value:
+            A two-element tuple. The first element is a new list with the result
+            of processing. The second one is a list of `MatchObject`s found
+            during processing.
+
+        """
         result = list_[:]
         matches = []
         while True:
-            m = self.match(result)
+            m = self.search(result)
             if not m:
                 break
-
             matches.append(m)
 
             body = self.body
-            for i in range(m.end()+1):
+            for i in range(m.end + 1):
                 if i in self.mapping:
-                    elem = result[m.start() + i]
+                    elem = result[m.start + i]
                     body = body.replace('{%s}' % i, self.mapping[i](elem))
+            result[m.start:m.end+1] = [body]
 
-            result[m.start():m.end()+1] = [body]
         return result, matches
 
+    def search(self, list_):
+        """Search for a sequence of elements matching the format string
 
-def _process_rule(rule, meta):
-    pattern, body = [x.strip() for x in rule.split('=')]
+        Returns a MatchObject instance encapsulating the range of the first
+        sequence of matching elements.
+
+        """
+        cmp_fun = lambda f, x: f is None or f(x)
+        list_len = len(list_)
+        pattern_len = len(self.pattern)
+
+        if self.pattern[0] == '^':
+            # Match only at the beginning of list_
+            bool_list = map(cmp_fun, self.pattern[1:], list_)
+            if list_len >= pattern_len - 1 and all(bool_list):
+                return MatchObject(0, pattern_len - 1)
+        elif list_len >= pattern_len:
+            # Run through the list looking for a matching sequence of elements
+            for i in range(list_len - pattern_len + 1):
+                bool_list = map(cmp_fun, self.pattern, list_[i:])
+                if all(bool_list):
+                    return MatchObject(i, pattern_len)
+        return None
+
+
+def _process_string(format_str, meta):
+    pattern, body = [x.strip() for x in format_str.split('=')]
 
     pattern, mapping = _build_pattern(pattern, meta)
     body = _build_body(body, mapping, meta)
