b83acc85a5c490f91e866a5dba79acfa1fcb9ef5
Alexei Sholik <alcosholik@gmail.com>
2011-12-09 21:08:07 +0200

    Update names in the test_lisp.py

--- test_lisp.py
+++ test_lisp.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-"""A test suite for listre module"""
+"""A test suite for the lisp module"""
 
 import unittest
 
-import listre
+import lisp
 
 
 ORDERS = ['', 'millón', 'billón', 'trillón']
@@ -31,150 +31,165 @@ class TestAnchors(unittest.TestCase):
     """
 
     def setUp(self):
-        self.lr = listre.ListreObject("^ 1 mil = mil")
+        self.lr = lisp.LispObject("^ 1 mil = mil")
 
-    def test_match(self):
+    def test_search(self):
         for list_ in [['1', 'mil']]:#, ['1', ' mil'], ['1', ' mil ']]:
-            self.assertTrue(self.lr.match(list_))
+            self.assertTrue(self.lr.search(list_))
+            self.assertEqual((0, 1), self.lr.search(list_).span)
 
-    def test_not_match(self):
+    def test_not_search(self):
         for list_ in [['10', '1', 'mil'], ['1', '1', 'mil'], ['mil']]:
-            self.assertFalse(self.lr.match(list_))
+            self.assertFalse(self.lr.search(list_))
 
 
 class TestOrder(unittest.TestCase):
     """Test order finding and substitution"""
 
     def setUp(self):
-        self.lr = listre.ListreObject("^ 1 <order> = un <order>", meta=lr_meta)
+        self.lr = lisp.LispObject("^ 1 <order> = un <order>", meta=lr_meta)
 
-    def test_anchored_match(self):
-        self.assertTrue(self.lr.match(['1', 1, 'bla']))
+    def test_anchored_search(self):
+        list_ = ['1', 1, 'bla']
+        self.assertTrue(self.lr.search(list_))
+        self.assertEqual((0, 1), self.lr.search(list_).span)
 
-    def test_normal_match(self):
-        lr = listre.ListreObject("1 <order> = un <order>", meta=lr_meta)
-        self.assertTrue(lr.match(['bla', '1', 2]))
+    def test_normal_search(self):
+        lr = lisp.LispObject("1 <order> = un <order>", meta=lr_meta)
+        list_ = ['bla', '1', 2]
+        self.assertTrue(lr.search(list_))
+        self.assertEqual((1, 2), lr.search(list_).span)
 
-    def test_not_match(self):
-        self.assertFalse(self.lr.match(['', '1', 1]))
+    def test_not_search(self):
+        self.assertFalse(self.lr.search(['', '1', 1]))
 
     def test_sub_sucess(self):
         self.assertEqual(['un billón', 'bla', 'bla'],
-                         self.lr.sub(['1', 2, 'bla', 'bla']))
+                         self.lr.sub(['1', 2, 'bla', 'bla'])[0])
 
     def test_sub_fail(self):
-        self.assertEqual(['', '1', 1, 'bla'], self.lr.sub(['', '1', 1, 'bla']))
+        self.assertEqual(['', '1', 1, 'bla'], self.lr.sub(['', '1', 1, 'bla'])[0])
 
 
 class TestConsecutive(unittest.TestCase):
-    """Consecutive elements have to match as well"""
+    """Consecutive elements have to search as well"""
 
     def setUp(self):
-        self.lr = listre.ListreObject("100 1 mil = ciento un mil")
+        self.lr = lisp.LispObject("100 1 mil = ciento un mil")
 
-    def test_match(self):
-        self.assertTrue(self.lr.match(['bla', '100', '1', 'mil']))
+    def test_search(self):
+        list_ = ['bla', '100', '1', 'mil']
+        self.assertTrue(self.lr.search(list_))
+        self.assertEqual((1, 3), self.lr.search(list_).span)
 
-    def test_not_match(self):
-        self.assertFalse(self.lr.match(['100', '', '1', 'mil']))
+    def test_not_search(self):
+        self.assertFalse(self.lr.search(['100', '', '1', 'mil']))
 
     def test_sub(self):
-        self.assertEqual(['ciento un mil'], self.lr.sub(['100', '1', 'mil']))
+        self.assertEqual(['ciento un mil'], self.lr.sub(['100', '1', 'mil'])[0])
 
 
 class TestAlternative(unittest.TestCase):
-    """Alternative matches are handy"""
+    """Alternative searches are handy"""
 
     def setUp(self):
-        self.lr = listre.ListreObject("100 1 <order> = "
+        self.lr = lisp.LispObject("100 1 <order> = "
                                  "ciento un <order>",
                                  meta=lr_meta)
 
-    def test_match_1(self):
-        self.assertTrue(self.lr.match(['bla', '100', '1', 'mil']))
+    def test_search_1(self):
+        list_ = ['bla', '100', '1', 'mil']
+        self.assertTrue(self.lr.search(list_))
+        self.assertEqual((1, 3), self.lr.search(list_).span)
 
-    def test_match_2(self):
-        self.assertTrue(self.lr.match(['bla', '100', '1', 2]))
+    def test_search_2(self):
+        list_ = ['bla', 'bla', '100', '1', 2]
+        self.assertTrue(self.lr.search(list_))
+        self.assertEqual((2, 4), self.lr.search(list_).span)
 
-    def test_not_match(self):
-        self.assertFalse(self.lr.match(['100', '1', 'ding']))
+    def test_not_search(self):
+        self.assertFalse(self.lr.search(['100', '1', 'ding']))
 
     def test_sub_1(self):
         self.assertEqual(['bla', 'ciento un mil', 'bla'],
-                         self.lr.sub(['bla', '100', '1', 'mil', 'bla']))
+                         self.lr.sub(['bla', '100', '1', 'mil', 'bla'])[0])
 
     def test_sub_2(self):
         self.assertEqual(['bla', 'ciento un trillón', 'bla'],
-                         self.lr.sub(['bla', '100', '1', 3, 'bla']))
+                         self.lr.sub(['bla', '100', '1', 3, 'bla'])[0])
 
 
 class TestAlternative2(unittest.TestCase):
     """This time, only one part of the expression is alternating"""
 
     def setUp(self):
-        self.lr = listre.ListreObject("1 <order> = un <order>",
+        self.lr = lisp.LispObject("1 <order> = un <order>",
                                       meta=lr_meta)
 
     def test_sub_mil(self):
-        self.assertEqual(['un mil'], self.lr.sub(['1', 'mil']))
+        self.assertEqual(['un mil'], self.lr.sub(['1', 'mil'])[0])
 
     def test_sub_millon(self):
-        self.assertEqual(['un millón'], self.lr.sub(['1', 1]))
+        self.assertEqual(['un millón'], self.lr.sub(['1', 1])[0])
 
 
 class TestModifier(unittest.TestCase):
     """Modifiers are handy"""
 
     def setUp(self):
-        self.lr = listre.ListreObject("<gt_1> <order> = <gt_1> <order,pl>",
+        self.lr = lisp.LispObject("<gt_1> <order> = <gt_1> <order,pl>",
                                  meta=lr_meta)
 
-    def test_match(self):
-        self.assertTrue(self.lr.match(['bla', '2', 1]))
+    def test_search(self):
+        list_ = ['bla', '2', 1]
+        self.assertTrue(self.lr.search(list_))
+        self.assertEqual((1, 2), self.lr.search(list_).span)
 
-    def test_not_match(self):
-        self.assertFalse(self.lr.match(['1', 1]))
+    def test_not_search(self):
+        self.assertFalse(self.lr.search(['1', 1]))
 
     def test_2(self):
-        self.assertEqual(['dos millones'], self.lr.sub(['2', 1]))
+        self.assertEqual(['dos millones'], self.lr.sub(['2', 1])[0])
 
     def test_11(self):
-        self.assertEqual(['once billones'], self.lr.sub(['11', 2]))
+        self.assertEqual(['once billones'], self.lr.sub(['11', 2])[0])
 
     def test_90(self):
-        self.assertEqual(['noventa trillones'], self.lr.sub(['90', 3]))
+        self.assertEqual(['noventa trillones'], self.lr.sub(['90', 3])[0])
 
 
 class TestLookup(unittest.TestCase):
     def setUp(self):
-        self.lr = listre.ListreObject("_ <order> = _ <order, pl>",
+        self.lr = lisp.LispObject("_ <order> = _ <order, pl>",
                                       meta=lr_meta)
 
-    def test_match(self):
+    def test_search(self):
         for key in lr_meta["_lookup"]:
-            self.assertTrue(self.lr.match([str(key), 1]))
+            list_ = [str(key), 1]
+            self.assertTrue(self.lr.search(list_))
+            self.assertEqual((0, 1), self.lr.search(list_).span)
 
-    def test_not_match(self):
+    def test_not_search(self):
         for num in ['2', '10', '25']:
-            self.assertFalse(self.lr.match([num, 1]))
+            self.assertFalse(self.lr.search([num, 1]))
 
     def test_lookup_1(self):
-        self.assertEqual(['un millones'], self.lr.sub(['1', 1]))
+        self.assertEqual(['un millones'], self.lr.sub(['1', 1])[0])
 
     def test_lookup_21(self):
-        self.assertEqual(['veintiún mil'], self.lr.sub(['21', 'mil']))
+        self.assertEqual(['veintiún mil'], self.lr.sub(['21', 'mil'])[0])
 
     def test_lookup_100_combined(self):
         self.assertEqual(['cien mil', 'cien millones'],
-                         self.lr.sub(['100', 'mil', '100', 1]))
+                         self.lr.sub(['100', 'mil', '100', 1])[0])
 
     def test_lookup_100_1(self):
         self.assertEqual(['cien mil'],
-                         self.lr.sub(['100', 'mil']))
+                         self.lr.sub(['100', 'mil'])[0])
 
     def test_lookup_100_2(self):
         self.assertEqual(['cien millones'],
-                         self.lr.sub(['100', 1]))
+                         self.lr.sub(['100', 1])[0])
 
 
 RU_ORDERS = ['', 'тысяча', 'миллион', 'миллиард', 'триллион']
@@ -206,38 +221,40 @@ ru_meta = {
 
 class TestRussian(unittest.TestCase):
     def test_anchor(self):
-        lr = listre.ListreObject("^ 1 <order> = <order>", ru_meta)
+        lr = lisp.LispObject("^ 1 <order> = <order>", ru_meta)
 
-        self.assertTrue(lr.match(['1', 1]))
-        self.assertFalse(lr.match(['', '1', 1]))
+        list_ = ['1', 1]
+        self.assertTrue(lr.search(list_))
+        self.assertEqual((0, 1), lr.search(list_).span)
+        self.assertFalse(lr.search(['', '1', 1]))
 
-        self.assertEqual(['миллион'], lr.sub(['1', 2]))
-        self.assertEqual(['тысяча'], lr.sub(['1', 1]))
+        self.assertEqual(['миллион'], lr.sub(['1', 2])[0])
+        self.assertEqual(['тысяча'], lr.sub(['1', 1])[0])
 
     def test_2_to_4(self):
-        lr = listre.ListreObject("<2_to_4> <order> = <2_to_4> <order, pl_1>", ru_meta)
+        lr = lisp.LispObject("<2_to_4> <order> = <2_to_4> <order, pl_1>", ru_meta)
 
         self.assertEqual(['два миллиарда'],
-                         lr.sub(['2', 3]))
+                         lr.sub(['2', 3])[0])
 
         self.assertEqual(['bla', 'четыре миллиона'],
-                         lr.sub(['bla', '4', 2]))
+                         lr.sub(['bla', '4', 2])[0])
 
         self.assertEqual(['bla', 'три тысячи', 'bla'],
-                         lr.sub(['bla', '3', 1, 'bla']))
+                         lr.sub(['bla', '3', 1, 'bla'])[0])
 
         self.assertEqual(['два миллиарда', 'четыре миллиона', 'два тысячи', 'три триллиона'],
-                         lr.sub(['2', 3, '4', 2, '2', 1, '3', 4]))
+                         lr.sub(['2', 3, '4', 2, '2', 1, '3', 4])[0])
 
     def test_1_thousand(self):
-        lr = listre.ListreObject("1 <thousand> = одна тысяча", ru_meta)
+        lr = lisp.LispObject("1 <thousand> = одна тысяча", ru_meta)
 
-        self.assertEqual(['одна тысяча'], lr.sub(['1', 1]))
+        self.assertEqual(['одна тысяча'], lr.sub(['1', 1])[0])
 
     def test_2_thousand(self):
-        lr = listre.ListreObject("2 <thousand> = две тысячи", ru_meta)
+        lr = lisp.LispObject("2 <thousand> = две тысячи", ru_meta)
 
-        self.assertEqual(['две тысячи'], lr.sub(['2', 1]))
+        self.assertEqual(['две тысячи'], lr.sub(['2', 1])[0])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
