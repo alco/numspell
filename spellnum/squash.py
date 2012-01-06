92f65624dd1c4c68b056aa09ce5ed7198ef73b5f
Alexei Sholik <alcosholik@gmail.com>
2012-01-06 11:33:27 +0200

    Extract unit-tests for squash into a separate file

--- spellnum/squash.py
+++ spellnum/squash.py
@@ -1,7 +1,8 @@
 __all__ = ['squash']
 
+
 from operator import add, eq
-import unittest
+
 
 def squash(predicate, list_):
     """Squash multiple consecutive list elements into one
@@ -53,45 +54,3 @@ def squash(predicate, list_):
         else:
             base = n
     return result
-
-
-### TESTS ###
-
-def isint(x):
-    return type(x) is int
-
-
-class SquashTest(unittest.TestCase):
-    def test_empty_list(self):
-        list_ = []
-        self.assertEqual(list_, squash(isint, list_))
-
-    def test_one_element(self):
-        for list_ in [[1], ['a'], [None], '', ' ']:
-            self.assertEqual(list_, squash(isint, list_))
-
-    def test_no_dupes(self):
-        list_ = [1, '2', 3, '4', ' ', 5, '6', ' ', 7, '8', '6', 9]
-        self.assertEqual(list_, squash(isint, list_))
-
-    def test_all_dupes(self):
-        for list_ in [[1] * 10, [1, '', ' ', ' ', 2, '', 3]]:
-            self.assertEqual([1], squash(isint, list_))
-
-    def test_range(self):
-        list_ = [' ', '', 1, 2, 3, '', 4, ' ']
-        self.assertEqual([' ', '', 1, ' '], squash(isint, list_))
-
-    def test_known_inputs(self):
-        known = [
-            ([2, 1, 0, 'a'], [2, 'a']),
-            (['a', 3, 2, 'b'], ['a', 3, 'b']),
-            ([2, 1], [2]),
-            (['a', 'b'], ['a', 'b']),
-            ([1, 'a', 2, 'b', 3, 4], [1, 'a', 2, 'b', 3]),
-        ]
-        for given, answer in known:
-            self.assertEqual(answer, squash(isint, given))
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ test/test_squash.py
@@ -0,0 +1,43 @@
+import unittest
+from squash import squash
+
+
+def isint(x):
+    return type(x) is int
+
+
+class SquashTest(unittest.TestCase):
+    def test_empty_list(self):
+        list_ = []
+        self.assertEqual(list_, squash(isint, list_))
+
+    def test_one_element(self):
+        for list_ in [[1], ['a'], [None], '', ' ']:
+            self.assertEqual(list_, squash(isint, list_))
+
+    def test_no_dupes(self):
+        list_ = [1, '2', 3, '4', ' ', 5, '6', ' ', 7, '8', '6', 9]
+        self.assertEqual(list_, squash(isint, list_))
+
+    def test_all_dupes(self):
+        for list_ in [[1] * 10, [1, '', ' ', ' ', 2, '', 3]]:
+            self.assertEqual([1], squash(isint, list_))
+
+    def test_range(self):
+        list_ = [' ', '', 1, 2, 3, '', 4, ' ']
+        self.assertEqual([' ', '', 1, ' '], squash(isint, list_))
+
+    def test_known_inputs(self):
+        known = [
+            ([2, 1, 0, 'a'], [2, 'a']),
+            (['a', 3, 2, 'b'], ['a', 3, 'b']),
+            ([2, 1], [2]),
+            (['a', 'b'], ['a', 'b']),
+            ([1, 'a', 2, 'b', 3, 4], [1, 'a', 2, 'b', 3]),
+        ]
+        for given, answer in known:
+            self.assertEqual(answer, squash(isint, given))
+
+
+if __name__ == '__main__':
+    unittest.main()
