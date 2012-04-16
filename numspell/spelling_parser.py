4bfac5d7fba395409fa617ddfdc77e7c833b3c1d
Alexei Sholik <alcosholik@gmail.com>
2012-04-16 23:30:05 +0300

    Adjust the spelling predicate format

--- numspell/spelling_parser.py
+++ numspell/spelling_parser.py
@@ -1,11 +1,33 @@
 import re
 import rule
 
-class Spelling(object):
-    def __init__(self, string):
-        self.decomp_rules = parse_decomp_rules(string)
-        self.numbers = parse_numbers(string)
-        self.orders = parse_orders(string)
+
+class Predicate(object):
+    def __init__(self, name, value, table):
+        self.name = name
+        self.value = value
+        self.table = table
+        self.clauses = []
+
+    def add_clause(self, line):
+        self.clauses.append(line)
+
+    def __repr__(self):
+        return '<%s: "%s" %s\t%s>' % (self.name, self.value, self.table, self.clauses)
+
+
+class Modifier(object):
+    def __init__(self, name):
+        """docstring for __init__"""
+        self.name = name
+        self.clauses = []
+
+    def add_clause(self, line):
+        self.clauses.append(line)
+
+    def __repr__(self):
+        return '<%s>' % ([':%s %s' % (self.name, x) for x in self.clauses])
+
 
 def parse_numbers(lst):
     numbers = {}
@@ -22,6 +44,57 @@ def parse_decomp_rules(lst):
     return [rule.from_str(x) for x in lst]
 
 def parse_transform_rules(lst):
+    rules = []
+    while len(lst):
+        line = lst.pop(0)
+        if line == '---':
+            break
+        rules.append(line)
+
+    NORMAL            = 0
+    PARSING_PREDICATE = 1
+    PARSING_MODIFIER  = 2
+
+    pred_re = re.compile(r'^(\w+):\s*"(.+)"(?:\s+\*(\w+)\*)?$')
+    mod_re = re.compile(r'^:(\w+)$')
+
+    predicates = []
+    modifiers = []
+
+    pred = None
+    mod = None
+    state = NORMAL
+    while len(lst):
+        line = lst.pop(0)
+
+        match = pred_re.match(line)
+        if match:
+            state = PARSING_PREDICATE
+            pred = Predicate(*match.groups())
+            predicates.append(pred)
+            continue
+
+        match = mod_re.match(line)
+        if match:
+            state = PARSING_MODIFIER
+            mod = Modifier(match.group(1))
+            modifiers.append(mod)
+            continue
+
+        if state == PARSING_PREDICATE:
+            if line.find('=') > 0:
+                pred.add_clause(line)
+            else:
+                state = NORMAL
+        elif state == PARSING_MODIFIER:
+            if line.find('=') > 0:
+                mod.add_clause(line)
+            else:
+                state = NORMAL
+
+    print predicates
+    print modifiers
+
     return lst
 
 def parse_user_transform(lst):
@@ -40,15 +113,18 @@ def parse_section(name, contents):
 
 def parse_sections(string):
     sections = {}
-    section_re = re.compile(r"\[(\w+)\]")
+    section_re = re.compile(r"^\[(\w+)\]$", flags = re.MULTILINE)
 
     # Get a list of sections, each followed by its contents
     section_list = filter(bool, re.split(section_re, string))
     for i in range(0, len(section_list), 2):
         name = section_list[i]
-        contents = filter(bool, [x.strip() for x in section_list[i+1].split('\n')])
+        contents = [x.strip() for x in section_list[i+1].split('\n')]
+        # filter empty lines and comments
+        contents = [x for x in contents if x and not x.startswith('%')]
         sections[name] = parse_section(name, contents)
     return sections
+
 
 if __name__ == '__main__':
     import sys
